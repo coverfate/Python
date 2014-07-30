cb046819cc53cfba162763b83c353d2f7d4ee210
coverfate <819219677@qq.com>
2014-07-30 14:09:43 +0800

    queen

---
+++ queen.py
@@ -0,0 +1,22 @@
+def queens(num=8, state=()):
+	for pos in range(num):
+		# 0-7
+		if not cf(state, pos):
+			if len(state) == num - 1:
+				yield (pos,)
+			else:
+				for r in queens(num, state + (pos,)):
+					yield(pos,) + r
+
+
+def cf(state, nextx):
+	nexty = len(state)
+	for i in range(nexty):
+		if(abs(state[i] - nextx)in(0, nexty - i)):
+			return True
+	return False
+def test():
+	print(list(queens(4)))
+	input()
+
+if __name__ == '__main__':test()
